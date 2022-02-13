b072b13549d473096b8a80f1a2d6d75696b65aa4
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2022-02-13 23:08:37 +0530

    basic unit testing added

--- fibo.spec.js
+++ fibo.spec.js
@@ -3,6 +3,10 @@ test("Returns 0 When value is 0", () => {
     expect(fibo(0)).toBe(0);
 });
 
-test("Returns q When value is 0", () => {
+test("Returns 3 When value is 5", () => {
     expect(fibo(1)).toBe(1);
+});
+
+test("Returns 3 When value is 5", () => {
+    expect(fibo(0)).toBe(3);
 });
